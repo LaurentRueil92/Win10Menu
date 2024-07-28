6284b66115ec068dd0cde8b2b2f46cdf38670f8b
laurentRueil92 <laurent.molina@gmail.com>
2024-07-28 22:12:33 +0200

    Ajoutez des fichiers projet.

---
+++ Win10Menu/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ButtonWin11 = New System.Windows.Forms.Button()
+        Me.ButtonWin10 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ButtonWin11
+        '
+        Me.ButtonWin11.Location = New System.Drawing.Point(22, 19)
+        Me.ButtonWin11.Name = "ButtonWin11"
+        Me.ButtonWin11.Size = New System.Drawing.Size(404, 74)
+        Me.ButtonWin11.TabIndex = 0
+        Me.ButtonWin11.Text = "Button1"
+        Me.ButtonWin11.UseVisualStyleBackColor = True
+        '
+        'ButtonWin10
+        '
+        Me.ButtonWin10.Location = New System.Drawing.Point(22, 19)
+        Me.ButtonWin10.Name = "ButtonWin10"
+        Me.ButtonWin10.Size = New System.Drawing.Size(404, 74)
+        Me.ButtonWin10.TabIndex = 1
+        Me.ButtonWin10.Text = "Button1"
+        Me.ButtonWin10.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(448, 109)
+        Me.Controls.Add(Me.ButtonWin10)
+        Me.Controls.Add(Me.ButtonWin11)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(6)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Menu Contextuel"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ButtonWin11 As Button
+    Friend WithEvents ButtonWin10 As Button
+End Class
---
+++ Win10Menu/Form1.vb
@@ -0,0 +1,60 @@
+﻿Imports System.Diagnostics
+Imports Microsoft.Win32
+
+Public Class Form1
+    Const SubKey As String = "Software\Classes\CLSID"
+    Const DirKey As String = "{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}"
+    Const EntryKey As String = "\InprocServer32"
+    ' version sous GitHub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Gestion du menu Contextuel - L. Molina Dec 2023"
+        Me.ButtonWin11.Text = "Passer au Menu Contextuel de type Win 11"
+        Me.ButtonWin10.Text = "Passer au Menu Contextuel de type Win 10"
+        GetStatus()
+    End Sub
+
+    Private Sub ButtonWin11_Click(sender As Object, e As EventArgs) Handles ButtonWin11.Click
+        ActivateNewMenuWin11()
+    End Sub
+
+    Private Sub ButtonWin10_Click(sender As Object, e As EventArgs) Handles ButtonWin10.Click
+        ActivateOldMenuWin10()
+    End Sub
+    Sub ActivateOldMenuWin10()
+        Dim softwareKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64)
+        Dim key = softwareKey?.OpenSubKey(SubKey, True)
+        key = key?.CreateSubKey(DirKey & EntryKey, RegistryKeyPermissionCheck.ReadWriteSubTree)
+        key?.SetValue("", "")
+        MsgBox("Veuillez relancer Win 11 pour activer la modification !", vbOKOnly, "Redémarrage nécessaire")
+        GetStatus()
+    End Sub
+
+    Sub ActivateNewMenuWin11()
+        Dim softwareKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64)
+        Dim keyDel = softwareKey?.OpenSubKey(SubKey, True)
+        keyDel?.DeleteSubKeyTree(DirKey, RegistryKeyPermissionCheck.ReadWriteSubTree)
+        MsgBox("Veuillez relancer Win 11 pour activer la modification !", vbOKOnly, "Redémarrage nécessaire")
+        GetStatus()
+    End Sub
+
+    Private Function ReadRegistry()
+        Dim KeyValue As Integer = 0
+        Dim baseKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64)
+        Dim regkey = baseKey.OpenSubKey(SubKey & "\" & DirKey)
+        If regkey IsNot Nothing Then
+            KeyValue = 1
+        End If
+        Return KeyValue
+    End Function
+
+    Sub GetStatus()
+        If ReadRegistry() = 1 Then
+            Me.ButtonWin10.Visible = False
+            Me.ButtonWin11.Visible = True
+        Else
+            Me.ButtonWin10.Visible = True
+            Me.ButtonWin11.Visible = False
+        End If
+
+    End Sub
+End Class
